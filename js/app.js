8f2965bb127a25edcf7a6d08eb16c0ee2a11d70d
MilagrosQB <qbmilagros@gmail.com>
2016-10-09 16:23:02 -0500

    cambios aleatorios

--- js/app.js
+++ js/app.js
@@ -5,28 +5,30 @@ function main (){
   var opcionCita = document.getElementById("opcionCita");
   var opcionMeme = document.getElementById("opcionMeme");
   var opcionPastilla = document.getElementById("opcionPastilla");
-  opcionTexto.addEventListener("click",bloqueTexto);
-  opcionCita.addEventListener("click",bloqueCita);
-  opcionMeme.addEventListener("click",bloqueMeme);
-  opcionPastilla.addEventListener("click",bloquePastilla);  
+      opcionTexto.addEventListener("click",bloqueTexto);
+      opcionCita.addEventListener("click",bloqueCita);
+      opcionMeme.addEventListener("click",bloqueMeme);
+      opcionPastilla.addEventListener("click",bloquePastilla);  
 }
 
   var taskContainer = document.getElementById("tasks-container");
+  var boxpost = document.getElementById("boxpost");
   var divContenedor = document.createElement("div");
   var divTexto = document.createElement("div");
-  var titulo = document.createElement("input");
+//  var titulo = document.createElement("input");
   var textArea = document.createElement("textarea");
   var btnCerrar = document.createElement("button");
   var btnPublicar = document.createElement("button");
+  var horaPost = horaPost();
 
 function bloqueTexto(){
     taskContainer.insertBefore(divContenedor,taskContainer.childNodes[0]);
     divContenedor.insertBefore(divTexto, divContenedor.childNodes[0]);
-        titulo.placeholder= "Título de la publicación";
-    divTexto.insertBefore(titulo, divTexto.childNodes[1]);
+/*      titulo.placeholder= "Título de la publicación";
+        divTexto.insertBefore(titulo, divTexto.childNodes[1]); */
         textArea.setAttribute("rows", 3);
         textArea.setAttribute("cols", 1);
-        textArea.placeholder="Escribe un comentario"; 
+        textArea.placeholder="Escribe un comentario";
     divTexto.insertBefore(textArea, divTexto.childNodes[2]);    
     btnCerrar.innerHTML= "Cerrar";
         divTexto.insertBefore(btnCerrar, divTexto.childNodes[3]);
@@ -35,7 +37,22 @@ function bloqueTexto(){
         })
     btnPublicar.innerHTML= "Publicar";
         divTexto.insertBefore(btnPublicar, divTexto.childNodes[3]);
-}
+        btnPublicar.addEventListener("click", function(e){
+            e.preventDefault();
+/*          var inputTitulo = document.createElement("div");
+            inputTitulo.innerHTML = titulo.value;
+            boxpost.appendChild(inputTitulo);   */        
+          var inputTexto = document.createElement("div");
+            inputTexto.innerHTML = textArea.value;
+            boxpost.appendChild(inputTexto);
+          //inputTexto.setAttribute("id","inputTexto");
+          var date = new Date().toLocaleTimeString(navigator.language, {hour:"2-digit", minute:"2-digit"});
+          var dateBox = document.createElement("div");
+            dateBox.innerText = date;  
+            boxpost.appendChild(dateBox);          
+        })
+    }        
+
 
 function bloqueCita(){
     taskContainer.insertBefore(divContenedor,taskContainer.childNodes[0]);
@@ -43,8 +60,8 @@ function bloqueCita(){
         textArea.setAttribute("rows", 3);
         textArea.setAttribute("cols", 1);
         textArea.placeholder="Escribe una frase"; 
-    divTexto.insertBefore(textArea, divTexto.childNodes[1]);  
-        titulo.placeholder= "Autor de la Cita";
+/*    divTexto.insertBefore(textArea, divTexto.childNodes[1]);  
+        titulo.placeholder= "Autor de la Cita";*/
     divTexto.insertBefore(titulo, divTexto.childNodes[2]);
         btnCerrar.innerHTML= "Cerrar";
     divTexto.insertBefore(btnCerrar, divTexto.childNodes[3]);
@@ -53,7 +70,17 @@ function bloqueCita(){
         })     
     btnPublicar.innerHTML= "Publicar";
         divTexto.insertBefore(btnPublicar, divTexto.childNodes[3]);        
- }
+        btnPublicar.addEventListener("click", function(e){
+          e.preventDefault();         
+          var inputCita = document.createElement("div");
+            inputCita.innerHTML = textArea.value;
+            boxpost.appendChild(inputCita);
+          var date = new Date().toLocaleTimeString(navigator.language, {hour:"2-digit", minute:"2-digit"});
+          var dateBox = document.createElement("div");
+            dateBox.innerText = date;  
+            boxpost.appendChild(dateBox);             
+        })
+    }      
 
 function bloqueMeme(){
     taskContainer.insertBefore(divContenedor,taskContainer.childNodes[0]);
@@ -68,8 +95,19 @@ function bloqueMeme(){
           divContenedor.parentNode.removeChild(divContenedor);
         })     
         btnPublicar.innerHTML= "Publicar";
-    divTexto.insertBefore(btnPublicar, divTexto.childNodes[2]);        
- }
+    divTexto.insertBefore(btnPublicar, divTexto.childNodes[3]);        
+    btnPublicar.addEventListener("click", function(e){
+      e.preventDefault();
+          var inputUrl = document.createElement("div");
+          url.innerHTML = titulo.value;
+          boxpost.appendChild(inputUrl);
+          var date = new Date().toLocaleTimeString(navigator.language, {hour:"2-digit", minute:"2-digit"});
+          var dateBox = document.createElement("div");
+            dateBox.innerText = date;  
+            boxpost.appendChild(dateBox); 
+
+        })
+    }      
 
 function bloquePastilla(){
     taskContainer.insertBefore(divContenedor,taskContainer.childNodes[0]);
@@ -83,9 +121,19 @@ function bloquePastilla(){
         btnCerrar.addEventListener("click", function(){
           divContenedor.parentNode.removeChild(divContenedor);
         })    
-        btnPublicar.innerHTML= "Pastilla";
-    divTexto.insertBefore(btnPublicar, divTexto.childNodes[3]);        
- }
+        btnPublicar.innerHTML= "Publicar";
+    divTexto.insertBefore(btnPublicar, divTexto.childNodes[3]);
+        btnPublicar.addEventListener("click", function(e){
+          e.preventDefault();       
+          var inputPastilla = document.createElement("div");
+            inputPastilla.innerHTML = textArea.value;
+            boxpost.appendChild(inputPastilla);
+          var date = new Date().toLocaleTimeString(navigator.language, {hour:"2-digit", minute:"2-digit"});
+          var dateBox = document.createElement("div");
+            dateBox.innerText = date;  
+            boxpost.appendChild(dateBox);             
+        })
+    }    
 
 /*function Post(textArea, hora, titulo){
   this.textArea = textArea;
--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
-$(function () {
-  $('select').material_select();
-});
+function horaPost (){
+	var datoTiempo = new Date();
+	var dia = datoTiempo.getDate();
+	var mes = datoTiempo.getMonth();
+	var anio = datoTiempo.getFullYear();
+	var hora = datoTiempo.getHours();
+	var minuto = datoTiempo.getMinutes();
+	var segundo = datoTiempo.getSeconds();
+	return dia + "/" + mes + "/" + anio + "(" + hora + ":"+ minuto + ":"+ segundo + ")"             
+}
+
+
+// $(function () {
+//   $('select').material_select();
+// });
